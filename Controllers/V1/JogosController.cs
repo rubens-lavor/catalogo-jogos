063f0d6c41db8e0921268666ff89e183e00ad1e3
rubens-lavor <haryson07@gmail.com>
2021-05-29 23:10:28 -0300

    controller v1 comunicação

---
+++ Controllers/V1/JogosController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace catalogo_jogos_DIO.Controllers.V1
+{
+  [Route("api/V1/[controller]")]
+  [ApiController]
+  public class JogosController : ControllerBase
+  {
+
+    [HttpGet]
+    public async Task<ActionResult<List<object>>> Obter()
+    {
+      return Ok();
+    }
+
+
+    [HttpGet("{idJogo:guid}")]
+    public async Task<ActionResult<object>> Obter(Guid idJogo)
+    {
+      return Ok();
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult<object>> InserirJogo(){
+      return Ok();
+    }
+
+    [HttpPut("{idJogo:guid}")]
+    public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo){
+      return Ok();
+    }
+
+    [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+    public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco){
+      return Ok();
+    }
+
+    [HttpDelete("{idJogo:guid}")]
+    public async Task<ActionResult> ApagarJogo(Guid idJogo){
+      return Ok();
+    }
+
+
+  }
+}
